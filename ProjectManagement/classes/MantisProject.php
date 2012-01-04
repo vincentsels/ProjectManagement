5d943a496b64b354a9c354488038c9fd38cc4df4
Vincent Sels <vincent_sels@hotmail.com>
2012-01-04 21:39:58 +0100

    Some progress

--- ProjectManagement/classes/MantisProject.php
+++ ProjectManagement/classes/MantisProject.php
@@ -2,15 +2,18 @@
 
 class MantisProject {
 	public $project_name;
-	public $subprojects = array();
-	public $categories = array();
+	public $parent_project;
+	public $sub_projects;
+	public $categories;
 	
 	public function __construct( $p_projectname ) {
 		$this->project_name = $p_projectname;
+		$this->sub_projects = array();
+		$this->categories = array();
 	}
 	
 	public function est() {
-		foreach ( $this->subprojects as $subproject ) {
+		foreach ( $this->sub_projects as $subproject ) {
 			$t_est += $subproject->est();
 		}
 		foreach ( $this->categories as $category ) {
@@ -19,11 +22,33 @@ class MantisProject {
 		return $t_est;
 	}
 	
+	public function done() {
+		foreach ( $this->sub_projects as $subproject ) {
+			$t_done += $subproject->done();
+		}
+		foreach ( $this->categories as $category ) {
+			$t_done += $category->done();
+		}
+		return $t_done;
+	}
+	
+	public function todo() {
+		foreach ( $this->sub_projects as $subproject ) {
+			$t_todo += $subproject->todo();
+		}
+		foreach ( $this->categories as $category ) {
+			$t_todo += $category->todo();
+		}
+		return $t_todo;
+	}
+	
 	public function print_project() {
 		echo '<div class="project">';
 		
 		print_expand_icon_start( $this->project_name );
-		echo '<span class="project-title">Project: ' . $this->project_name . '</span> - completed: ' . $this->est();
+		echo '<span class="project-title">Project: ', $this->project_name, '</span> - ',
+			minutes_to_time( $this->done(), false ), ' / ', minutes_to_time( $this->est(), false ), 
+			' (',  minutes_to_time( $this->todo(), false ), ')';
 		print_expand_icon_end();
 		
 		print_expandable_div_start( $this->project_name );
@@ -31,7 +56,7 @@ class MantisProject {
 			$category->print_category();
 		}
 		
-		foreach ( $this->subprojects as $subproject ) {
+		foreach ( $this->sub_projects as $subproject ) {
 			$subproject->print_project();
 		}
 		print_expandable_div_end();
@@ -44,26 +69,43 @@ class MantisCategory {
 	public $category_name;
 	public $parent_project;
 	public $unique_id;
-	public $bugs = array();
+	public $bugs;
 	
 	public function __construct( $p_category_name, $p_parent_project_name ) {
 		$this->category_name = $p_category_name;
 		$this->parent_project = $p_parent_project_name;
 		$this->unique_id = $this->parent_project . '_' . $this->category_name;
+		$this->bugs = array();
 	}
 	
 	public function est() {
 		foreach ( $this->bugs as $bug ) {
-			$t_est += $bug->est;
+			$t_est += $bug->est();
 		}
 		return $t_est;
+	}
+	
+	public function done() {
+		foreach ( $this->bugs as $bug ) {
+			$t_done += $bug->done();
+		}
+		return $t_done;
+	}
+	
+	public function todo() {
+		foreach ( $this->bugs as $bug ) {
+			$t_todo += $bug->todo();
+		}
+		return $t_todo;
 	}
 	
 	public function print_category() {
 		echo '<div class="category">';
 		
 		print_expand_icon_start( $this->unique_id );
-		echo '<span class="category-title">Category: ' . $this->category_name . '</span> - completed: ' . $this->est();
+		echo '<span class="category-title">Category: ', $this->category_name, '</span> - ',
+			minutes_to_time( $this->done(), false ), ' / ', minutes_to_time( $this->est(), false ), 
+			' (',  minutes_to_time( $this->todo(), false ), ')';
 		print_expand_icon_end();
 		
 		print_expandable_div_start( $this->unique_id );
@@ -79,19 +121,49 @@ class MantisCategory {
 class MantisBug {
 	public $bug_id = -1;
 	public $handler_id = 1;
-	public $est = 0;
-	public $done = 0;
-	public $todo = 0;
+	public $est;
+	public $done;
+	public $todo;
 	
-	public function __construct( $p_bug_id, $p_est = 1 ) {
+	public function __construct( $p_bug_id ) {
 		$this->bug_id = $p_bug_id;
-		$this->est = $p_bug_id;
+		$this->est = array();
+		$this->done = array();
+		$this->todo = array();
+	}
+	
+	public function est() {
+		foreach ( $this->est as $t_value ) {
+			$t_est += $t_value;
+		}
+		return $t_est;
+	}
+	
+	public function done() {
+		foreach ( $this->done as $t_value ) {
+			$t_done += $t_value;
+		}
+		return $t_done;
+	}
+	
+	public function todo() {
+		$t_worktypes = MantisEnum::getAssocArrayIndexedByValues( plugin_config_get( 'work_types' ) );
+		foreach ( $t_worktypes as $t_work_type => $t_value ) {
+			if ( !empty( $this->todo[$t_work_type] ) ) {
+				$t_todo += $this->todo[$t_work_type];
+			} else if ( !empty( $this->est[$t_work_type] ) ) {
+				$t_todo += ( $this->est[$t_work_type] - $this->done[$t_work_type] );
+			}
+		}
+		return max( $t_todo, 0 );
 	}
 	
 	public function print_bug() {
 		echo '<div class="bug">';
 		
-		echo 'Bug, id: ' . $this->bug_id . ' - progress: ' . $this->est;
+		echo 'Bug, id: ' , $this->bug_id , ' - ', 
+			minutes_to_time( $this->done(), false ), ' / ', minutes_to_time( $this->est(), false ), 
+			' (',  minutes_to_time( $this->todo(), false ), ')';
 		
 		echo '</div>';
 	}
--- ProjectManagement/pages/resource_allocation_page.php
+++ ProjectManagement/pages/resource_allocation_page.php
@@ -7,6 +7,82 @@ html_page_top2();
 
 print_pm_reports_menu( 'resource_allocation_page' );
 
+$t_target_version = '5.0.0';
+$t_query = "SELECT pp.name as parent_project, pc.name as project_name, c.name as category_name, b.id, b.handler_id, w.work_type, w.minutes_type, sum(w.minutes) as minutes
+  FROM mantis_bug_table b
+  JOIN mantis_project_table pc ON b.project_id = pc.id
+  JOIN mantis_category_table c ON b.category_id = c.id
+  LEFT OUTER JOIN mantis_project_hierarchy_table h ON pc.id = h.child_id
+  LEFT OUTER JOIN mantis_project_table pp ON h.parent_id = pp.id
+  LEFT OUTER JOIN mantis_plugin_projectmanagement_work_table w ON b.id = w.bug_id " .
+# WHERE b.target_version = '$t_target_version'
+ "GROUP BY pp.name, pc.name, c.name, b.id, b.handler_id, w.work_type, w.minutes_type";
+$t_result = db_query_bound( $t_query );
+$t_rownum = db_num_rows( $t_result );
+
+$t_all_projects = array(); # Array containing all projects
+$t_main_projects = array(); # Array containing only the top projects (who don't have a parent project)
+
+# Start creation of the objects
+for ($i = 0; $i < $t_rownum; $i++) {
+	$row = db_fetch_array( $t_result );
+	
+	# Check whether this project already exists and if not, create it
+	if ( array_key_exists( $row['project_name'], $t_all_projects ) ) {
+		$t_project = $t_all_projects[$row['project_name']];
+	} else {
+		$t_project = new MantisProject( $row['project_name'] );
+		$t_all_projects[$row['project_name']] = $t_project;
+	}
+	
+	# Check whether this category already exists in this project and if not, create it
+	if ( !is_null( $t_project->categories ) && array_key_exists( $row['category_name'], $t_project->categories ) ) {
+		$t_category = $t_project->categories[$row['category_name']];
+	} else {
+		$t_category = new MantisCategory( $row['category_name'], $row['project_name'] );
+		$t_project->categories[$row['category_name']] = $t_category;
+	}
+	
+	# Check whether this ticket already exists in this category and if not, add it
+	if ( !is_null( $t_category->bugs ) && array_key_exists( $row['id'], $t_category->bugs ) ) {
+		$t_bug = $t_category->bugs[$row['id']];
+	} else {
+		$t_bug = new MantisBug( $row['id'] );
+		$t_category->bugs[$row['id']] = $t_bug;
+	}
+	
+	# Set the est, done or todo for this bug and worktype
+	if ( $row['minutes_type'] == PLUGIN_PM_EST ) {
+		$t_bug->est[$row['work_type']] = $row['minutes'];
+	} else if ( $row['minutes_type'] == PLUGIN_PM_DONE ) {
+		$t_bug->done[$row['work_type']] = $row['minutes'];
+	} else if ( $row['minutes_type'] == PLUGIN_PM_TODO ) {
+		$t_bug->todo[$row['work_type']] = $row['minutes'];
+	}
+	
+	# Set the handler_id
+	if ( !empty( $row['handler_id'] ) ) {
+		$t_bug->handler_id = $row['handler_id'];
+	}
+	
+	# Set the parent project
+	if ( !empty( $row['parent_project'] ) ) {
+		$t_project->parent_project = $row['parent_project'];
+	}
+}
+
+# Set main and sub projects
+foreach ( $t_all_projects as $t_project_name => $t_project ) {
+	if ( !isset( $t_project->parent_project ) ) {
+		$t_main_projects[] = $t_project;
+	} else {
+		$t_all_projects[$t_project->parent_project]->sub_projects[] = $t_project;
+	}
+}
+
+/*
+ * test set
+ * 
 $t_projects = array();
 $t_project1 = new MantisProject( 'Project 1' );
 $t_category1 = new MantisCategory( 'Category 1', 'Project 1' );
@@ -24,9 +100,10 @@ $t_category3->bugs[] = new MantisBug( 6 );
 $t_subproject->categories[] = $t_category3;
 $t_project1->subprojects[] = $t_subproject;
 $t_projects[] = $t_project1;
+*/
 
-foreach ( $t_projects as $t_project ) {
-	$t_project->print_project();
+foreach ( $t_main_projects as $t_main_project ) {
+	$t_main_project->print_project();
 	echo '<p>';
 }
 
