14ad59d28c150457b79e08d66b5b381e2b476c01
Vincent Sels <vincent_sels@hotmail.com>
2013-01-03 17:29:48 +0100

    Rearranged configuration page and added array of unavailability types

--- ProjectManagement/pages/config_page.php
+++ ProjectManagement/pages/config_page.php
@@ -12,15 +12,15 @@ print_pm_config_menu( 'config_page' );
 <br/>
 <form method="post" action="<?php echo plugin_page( 'config_update' ) ?>">
 	<?php echo form_security_field( 'plugin_ProjectManagement_config_update' ) ?>
-	<table class="width100" align="center" cellspacing="1">
+	<table class="width75" align="center" cellspacing="1">
 		<tr>
 			<td class="form-title"
-				colspan="2"><?php echo plugin_lang_get( 'title' ), ': ', plugin_lang_get( 'configuration' ) ?></td>
+				colspan="2"><?php echo plugin_lang_get( 'time_registration' ) ?></td>
 		</tr>
 		<tr <?php echo helper_alternate_class() ?>>
-			<td width="25%" class="category"><?php echo plugin_lang_get( 'work_types' ) ?><br/>
+			<td width="30%" class="category"><?php echo plugin_lang_get( 'work_types' ) ?><br/>
 				<span class="small"><?php echo plugin_lang_get( 'work_types_info' ) ?></span></td>
-			<td width="75%"><input type="text" size="100" maxlength="200" name="work_types" id="work_types"
+			<td><input type="text" class="large-textbox" maxlength="200" name="work_types" id="work_types"
 								   value="<?php echo plugin_config_get( 'work_types' ) ?>"></td>
 		</tr>
 		<tr class="spacer"/>
@@ -53,25 +53,31 @@ print_pm_config_menu( 'config_page' );
 		<tr <?php echo helper_alternate_class() ?>>
 			<td class="category"><?php echo plugin_lang_get( 'work_type_thresholds' ) ?><br/>
 				<span class="small"><?php echo plugin_lang_get( 'work_type_thresholds_info' ) ?></span></td>
-			<td><input type="text" size="100" maxlength="200" name="work_type_thresholds" id="work_type_thresholds"
+			<td><input type="text" class="large-textbox" maxlength="200" name="work_type_thresholds" id="work_type_thresholds"
 					   value="<?php var_export( plugin_config_get( 'work_type_thresholds' ) ) ?>"></td>
 		</tr>
 		<tr class="spacer"/>
 		<tr <?php echo helper_alternate_class() ?>>
 			<td class="category"><?php echo plugin_lang_get( 'finish_upon_resolving' ) ?><br/>
 				<span class="small"><?php echo plugin_lang_get( 'finish_upon_resolving_info' ) ?></span></td>
-			<td><input type="text" size="100" maxlength="200" name="finish_upon_resolving" id="finish_upon_resolving"
+			<td><input type="text" class="large-textbox" maxlength="200" name="finish_upon_resolving" id="finish_upon_resolving"
 					   value="<?php var_export( plugin_config_get( 'finish_upon_resolving' ) ) ?>"></td>
 		</tr>
 		<tr <?php echo helper_alternate_class() ?>>
 			<td class="category"><?php echo plugin_lang_get( 'finish_upon_closing' ) ?><br/>
 				<span class="small"><?php echo plugin_lang_get( 'finish_upon_closing_info' ) ?></span></td>
-			<td><input type="text" size="100" maxlength="200" name="finish_upon_closing" id="finish_upon_closing"
+			<td><input type="text" class="large-textbox" maxlength="200" name="finish_upon_closing" id="finish_upon_closing"
 					   value="<?php var_export( plugin_config_get( 'finish_upon_closing' ) ) ?>"></td>
 		</tr>
-		<tr class="spacer"/>
+    </table><br />
+
+    <table class="width75" align="center" cellspacing="1">
+        <tr>
+            <td class="form-title"
+                colspan="2"><?php echo lang_get( 'access_levels' ) ?></td>
+        </tr>
 		<tr <?php echo helper_alternate_class() ?>>
-			<td class="category"><?php echo plugin_lang_get( 'view_time_registration_worksheet_threshold' ) ?></td>
+			<td width="30%" class="category"><?php echo plugin_lang_get( 'view_time_registration_worksheet_threshold' ) ?></td>
 			<td><select
 				name="view_registration_worksheet_threshold"><?php print_enum_string_option_list( 'access_levels', plugin_config_get( 'view_registration_worksheet_threshold' ) ) ?></select>
 			</td>
@@ -112,28 +118,32 @@ print_pm_config_menu( 'config_page' );
 				name="admin_threshold"><?php print_enum_string_option_list( 'access_levels', plugin_config_get( 'admin_threshold' ) ) ?></select>
 			</td>
 		</tr>
-		<tr class="spacer"/>
+    </table><br />
+
+    <table class="width75" align="center" cellspacing="1">
+        <tr>
+            <td class="form-title"
+                colspan="2"><?php echo plugin_lang_get( 'localization' ) ?></td>
+        </tr>
 		<tr <?php echo helper_alternate_class() ?>>
-			<td class="category"><?php echo plugin_lang_get( 'decimal_separator' ) ?></td>
-			<td><input type="text" size="2" maxlength="2" name="decimal_separator" id="decimal_separator"
+			<td width="30%" class="category"><?php echo plugin_lang_get( 'decimal_separator' ) ?></td>
+			<td><input type="text" class="small-textbox" maxlength="2" name="decimal_separator" id="decimal_separator"
 					   value="<?php echo plugin_config_get( 'decimal_separator' ) ?>"></td>
 		</tr>
 		<tr <?php echo helper_alternate_class() ?>>
 			<td class="category"><?php echo plugin_lang_get( 'thousand_separator' ) ?></td>
-			<td><input type="text" size="2" maxlength="2" name="thousand_separator" id="thousand_separator"
+			<td><input type="text" class="small-textbox" maxlength="2" name="thousand_separator" id="thousand_separator"
 					   value="<?php echo plugin_config_get( 'thousand_separator' ) ?>"></td>
 		</tr>
-		<tr class="spacer"/>
+    </table><br />
+
+    <table class="width75" align="center" cellspacing="1">
+        <tr>
+            <td class="form-title"
+                colspan="2"><?php echo plugin_lang_get( 'customer_section' ) ?></td>
+        </tr>
 		<tr <?php echo helper_alternate_class() ?>>
-			<td class="category"><?php echo plugin_lang_get( 'include_bugs_with_deadline' ) ?><br/>
-				<span class="small"><?php echo plugin_lang_get( 'include_bugs_with_deadline_warning' ) ?></span></td>
-			<td><input type="checkbox" name="include_bugs_with_deadline"
-					   id="include_bugs_with_deadline" <?php echo plugin_config_get( 'include_bugs_with_deadline' ) ? 'checked="checked" ' : '' ?>>
-				<?php echo plugin_lang_get( 'include_bugs_with_deadline_info' ) ?></span></td>
-		</tr>
-		<tr class="spacer"/>
-		<tr <?php echo helper_alternate_class() ?>>
-			<td class="category"><?php echo plugin_lang_get( 'enable_customer_payment_threshold' ) ?><br/>
+			<td width="30%" class="category"><?php echo plugin_lang_get( 'enable_customer_payment_threshold' ) ?><br/>
 				<span class="small"><?php echo plugin_lang_get( 'enable_customer_payment_threshold_info' ) ?></span>
 			</td>
 			<td><select
@@ -161,17 +171,37 @@ print_pm_config_menu( 'config_page' );
 				name="view_billing_threshold"><?php print_enum_string_option_list( 'access_levels', plugin_config_get( 'view_billing_threshold' ) ) ?></select>
 			</td>
 		</tr>
-		<tr class="spacer"/>
+    </table><br />
+
+    <table class="width75" align="center" cellspacing="1">
+        <tr>
+            <td class="form-title"
+                colspan="2"><?php echo plugin_lang_get( 'targets' ) ?></td>
+        </tr>
 		<tr <?php echo helper_alternate_class() ?>>
-			<td class="category"><?php echo plugin_lang_get( 'default_owner' ) ?><br/>
+			<td width="30%" class="category"><?php echo plugin_lang_get( 'default_owner' ) ?><br/>
 				<span class="small"><?php echo plugin_lang_get( 'default_owner_info' ) ?></span></td>
-			<td><input type="text" size="100" maxlength="200" name="default_owner" id="default_owner"
+			<td><input type="text" class="large-textbox" maxlength="200" name="default_owner" id="default_owner"
 					   value="<?php var_export( plugin_config_get( 'default_owner' ) ) ?>"></td>
 		</tr>
-		<tr class="spacer"/>
+    </table><br />
+
+    <table class="width75" align="center" cellspacing="1">
+        <tr>
+            <td class="form-title"
+                colspan="2"><?php echo plugin_lang_get( 'project_progress' ), ' & ', plugin_lang_get( 'resource_progress' ) ?></td>
+        </tr>
+        <tr <?php echo helper_alternate_class() ?>>
+            <td width="30%" class="category"><?php echo plugin_lang_get( 'include_bugs_with_deadline' ) ?><br/>
+                <span class="small"><?php echo plugin_lang_get( 'include_bugs_with_deadline_warning' ) ?></span></td>
+            <td><input type="checkbox" name="include_bugs_with_deadline"
+                       id="include_bugs_with_deadline" <?php echo plugin_config_get( 'include_bugs_with_deadline' ) ? 'checked="checked" ' : '' ?>>
+                <?php echo plugin_lang_get( 'include_bugs_with_deadline_info' ) ?></span></td>
+        </tr>
+        <tr class="spacer"/>
 		<tr <?php echo helper_alternate_class() ?>>
-			<td class="category"><?php echo plugin_lang_get( 'release_buffer' ) ?></td>
-			<td><input type="text" size="2" maxlength="2" name="release_buffer" id="release_buffer"
+			<td width="30%" class="category"><?php echo plugin_lang_get( 'release_buffer' ) ?></td>
+			<td><input type="text" class="small-textbox" maxlength="2" name="release_buffer" id="release_buffer"
 					   value="<?php echo plugin_config_get( 'release_buffer' ) ?>"></td>
 		</tr>
         <tr <?php echo helper_alternate_class() ?>>
@@ -218,12 +248,17 @@ print_pm_config_menu( 'config_page' );
                        value="<?php echo @$t_work_hours_per_day[7] ?>">
             </td>
         </tr>
-
+        <tr class="spacer"/>
+        <tr <?php echo helper_alternate_class() ?>>
+            <td class="category"><?php echo plugin_lang_get( 'unavailability_types' ) ?></td>
+            <td><input type="text" class="large-textbox" maxlength="200" name="unavailability_types" id="unavailability_types"
+                                   value="<?php echo plugin_config_get( 'unavailability_types' ) ?>"></td>
+        </tr>
 
 		<tr>
 			<td class="center" colspan="2"><input type="submit" value="<?php echo lang_get( 'update' ) ?>"/></td>
 		</tr>
-	</table>
+	</table><br />
 </form>
 
 <?php
